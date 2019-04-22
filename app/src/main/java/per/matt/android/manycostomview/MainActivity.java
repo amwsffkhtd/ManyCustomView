d786335270577204ee281298ea98d5c957858de6
Matt <matth@laxtongroup.com>
2019-04-22 09:47:10 +0800

    update test

--- app/src/main/java/per/matt/android/manycostomview/MainActivity.java
+++ app/src/main/java/per/matt/android/manycostomview/MainActivity.java
@@ -2,12 +2,24 @@ package per.matt.android.manycostomview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_common_background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_common_background = findViewById(R.id.btn_common_background);
+        btn_common_background.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
